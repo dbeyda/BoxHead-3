23c0d76862f568ca15102bda42bc93529f523e55
David Beyda <dbeyda@globo.com>
2018-06-23 23:00:21 -0300

    [Added] Movement in diagonal directions.

--- entities/player.lua
+++ entities/player.lua
@@ -1,12 +1,11 @@
 --! file: player.lua
 
 local screenWidth, screenHeight = love.graphics.getDimensions()
-local playerStep = 10
+local playerStep = 5
 
 Player = Object:extend()
 
 function Player:new()
-    self.test = math.random(1, 1000)
     self.x = 100
     self.y = 100
     self.health = 100
@@ -14,35 +13,53 @@ function Player:new()
     self.weaponList = {}
     self.width = 40
     self.height = 40
-    self.direction = 'south' --! north, west, south, east
+    self.direction = 'south' --! north, northeast, east, southest, south, southwest, west, northwest
 end
 
 
 function Player:update(dt)
-    if love.keyboard.isDown('right') then
-        self.direction = 'right'
-        local new_x = self.x + playerStep
-        if new_x >= 0 and new_x <= screenWidth-self.width then
-            self.x = new_x
-        end
+    local newX = self.x
+    local newY = self.y
+
+    if love.keyboard.isDown('right') and love.keyboard.isDown('up') then
+        self.direction = 'northeast'
+        newX = self.x + playerStep
+        newY = self.y - playerStep
+    elseif love.keyboard.isDown('right') and love.keyboard.isDown('down') then
+        self.direction = 'southeast'
+        newX = self.x + playerStep
+        newY = self.y + playerStep
+    elseif love.keyboard.isDown('left') and love.keyboard.isDown('up') then
+        self.direction = 'northwest'
+        newX = self.x - playerStep
+        newY = self.y - playerStep
+    elseif love.keyboard.isDown('left') and love.keyboard.isDown('down') then
+        self.direction = 'southwest'
+        newX = self.x - playerStep
+        newY = self.y + playerStep
+    elseif love.keyboard.isDown('right') then
+        self.direction = 'east'
+        newX = self.x + playerStep
+        newY = self.y
     elseif love.keyboard.isDown('left') then
-        self.direction = 'left'
-        local new_x = self.x - playerStep
-        if new_x >= 0 and new_x <= screenWidth then
-            self.x = new_x
-        end
+        self.direction = 'west'
+        newX = self.x - playerStep
+        newY = self.y
     elseif love.keyboard.isDown('up') then
-        self.direction = 'up'
-        local new_y = self.y - playerStep
-        if new_y >= 0 and new_y <= screenHeight then
-            self.y = new_y
-        end
+        self.direction = 'north'
+        newX = self.x
+        newY = self.y - playerStep
     elseif love.keyboard.isDown('down') then
-        self.direction = 'down'
-        local new_y = self.y + playerStep
-        if new_y >= 0 and new_y <= screenHeight-self.height then
-            self.y = new_y
-        end
+        self.direction = 'south'
+        newX = self.x
+        newY = self.y + playerStep
+    end
+
+    if xPositionIsValid(newX, self.width) then
+        self.x = newX
+    end
+    if yPositionIsValid(newY, self.height) then
+        self.y = newY
     end
 end
 
@@ -50,31 +67,39 @@ end
 function Player:draw()
     love.graphics.setColor(0, 105, 255, 255)
     love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-    --! draw the front of the player
+
+    --! draw the front of the player in the right direction
     local xx = 0
     local yy = 0
     local size = 10
-    if self.direction == 'up' then
+    if self.direction == 'north' then
         xx = self.x + (self.width/2) - size/2
         yy = self.y
-    elseif self.direction == 'down' then
+    elseif self.direction == 'south' then
         xx = self.x + (self.width/2) - size/2
         yy = self.y + self.height - size
-    elseif self.direction == 'right' then
+    elseif self.direction == 'east' then
         xx = self.x + self.width - size
         yy = self.y + self.height/2 - size/2
-    elseif self.direction == 'left' then
+    elseif self.direction == 'west' then
         xx = self.x
         yy = self.y + self.height/2 - size/2
+    elseif self.direction == 'northeast' then
+        xx = self.x + self.width - size
+        yy = self.y
+    elseif self.direction == 'northwest' then
+        xx = self.x
+        yy = self.y
+    elseif self.direction == 'southeast' then
+        xx = self.x + self.width - size
+        yy = self.y + self.height - size
+    elseif self.direction == 'southwest' then
+        xx = self.x
+        yy = self.y + self.height - size
     end
     love.graphics.setColor(50, 0, 0, 255)
     love.graphics.rectangle("fill", xx, yy, size, size)
 end
-
-function Player.keypress(self, key)
-
-end
-
 
 function Player:getDirection()
     return self.direction
@@ -82,4 +107,20 @@ end
 
 function Player:getWeapon()
     return self.currentWeapon
+end
+
+function xPositionIsValid(x, width)
+    local screenWidth, screenHeight = love.graphics.getDimensions()
+    if x >= 0 and x <= screenWidth-width then
+        return true
+    end
+    return False
+end
+
+function yPositionIsValid(y, height)
+    local screenWidth, screenHeight = love.graphics.getDimensions()
+    if y >= 0 and y <= screenHeight-height then
+        return true
+    end
+    return False
 end
--- entities/projectile.lua
+++ entities/projectile.lua
@@ -1,84 +1,56 @@
 --! file: projectile.lua
 
-local bulletStep = 10
-
-Player = Object:extend()
-
-function Player:new()
-    self.test = math.random(1, 1000)
-    self.x = 100
-    self.y = 100
-    self.health = 100
-    self.currentWeapon = 0
-    self.weaponList = {}
-    self.width = 40
-    self.height = 40
-    self.direction = 'down' --! up, down, left, right
-end
+local bulletStep = 15
 
 
 function Player:update(dt)
-    if love.keyboard.isDown('right') then
-        self.direction = 'right'
-        local new_x = self.x + step
+    if love.keyboard.isDown('right', 'up') then
+        self.direction = 'northeast'
+        local new_x = self.x + playerStep
         if new_x >= 0 and new_x <= screenWidth-self.width then
             self.x = new_x
         end
-    elseif love.keyboard.isDown('left') then
-        self.direction = 'left'
-        local new_x = self.x - step
+    elseif love.keyboard.isDown('right', 'down') then
+        self.direction = 'southeast'
+        local new_x = self.x - playerStep
         if new_x >= 0 and new_x <= screenWidth then
             self.x = new_x
         end
-    elseif love.keyboard.isDown('up') then
-        self.direction = 'up'
-        local new_y = self.y - step
+    elseif love.keyboard.isDown('left', 'up') then
+        self.direction = 'northwest'
+        local new_y = self.y - playerStep
         if new_y >= 0 and new_y <= screenHeight then
             self.y = new_y
         end
-    elseif love.keyboard.isDown('down') then
-        self.direction = 'down'
-        local new_y = self.y + step
+    elseif love.keyboard.isDown('left', 'down') then
+        self.direction = 'southwest'
+        local new_y = self.y + playerStep
         if new_y >= 0 and new_y <= screenHeight-self.height then
             self.y = new_y
         end
+    elseif love.keyboard.isDown('right') then
+        self.direction = 'east'
+        local newX = self.x + playerStep
+        if positionIsValid(newX, self.y, self.width, self.height) then
+            self.x = new_x
+        end
+    elseif love.keyboard.isDown('left') then
+        self.direction = 'west'
+        local newX = self.x - playerStep
+        if positionIsValid(newX, self.y, self.width, self.height) then
+            self.x = new_x
+        end
+    elseif love.keyboard.isDown('up') then
+        self.direction = 'north'
+        local newY = self.y - playerStep
+        if positionIsValid(self.x, newY, self.width, self.height) then
+            self.y = new_y
+        end
+    elseif love.keyboard.isDown('down') then
+        self.direction = 'south'
+        local newY = self.y + playerStep
+        if positionIsValid(self.x, newY, self.width, self.height) then
+            self.y = new_y
+        end
     end
-end
-
-
-function Player:draw()
-    love.graphics.setColor(0, 105, 255, 255)
-    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-    --! draw the front of the player
-    local xx = 0
-    local yy = 0
-    local size = 10
-    if self.direction == 'up' then
-        xx = self.x + (self.width/2) - size/2
-        yy = self.y
-    elseif self.direction == 'down' then
-        xx = self.x + (self.width/2) - size/2
-        yy = self.y + self.height - size
-    elseif self.direction == 'right' then
-        xx = self.x + self.width - size
-        yy = self.y + self.height/2 - size/2
-    elseif self.direction == 'left' then
-        xx = self.x
-        yy = self.y + self.height/2 - size/2
-    end
-    love.graphics.setColor(50, 205, 25, 255)
-    love.graphics.rectangle("fill", xx, yy, size, size)
-end
-
-function Player.keypress(self, key)
-
-end
-
-
-function Player:getDirection()
-    return self.direction
-end
-
-function Player:getWeapon()
-    return self.currentWeapon
 end
